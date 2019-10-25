6655a45b511f3f85e49ac234080d0743934d00aa
Jonathan Pabon <35584599+JonnyPabon@users.noreply.github.com>
2019-10-24 19:34:22 -0500

    DI, showing shared data and changing..

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from '../shared-data.service';
 
 @Component({
   selector: 'app-foo',
@@ -7,15 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooComponent implements OnInit {
 
-  constructor() { }
+  //DI via typescript automatic property
+  //access specifier of private 
+  constructor(private sdSvc: SharedDataService) { }
 
   ngOnInit() {
   }
+  //initialized because we wanted to see our binding work
+  //temporary property just to show the data binding was working
+  //sharedData = 'Foo Shared Data';
 
-  sharedData = 'Foo Shared Data';
+  //TS getter property - getter is we can run code when it is requested.
+  get sharedData() {
+    return this.sdSvc.data;
+  }
 
   //property that is being initialized
   changeSharedData = () => {
     console.log('changedSharedData');
+    this.sdSvc.data='Foo';
   }
 }
