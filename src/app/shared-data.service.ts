1370709bde5363beee9133fc24839de8393efe7e
Jonathan Pabon <35584599+JonnyPabon@users.noreply.github.com>
2019-10-24 19:59:56 -0500

    Getter and Setters

--- src/app/shared-data.service.ts
+++ src/app/shared-data.service.ts
@@ -7,5 +7,17 @@ export class SharedDataService {
 
   constructor() { }
 
-  data = 'Uninitialized';
+  //normal property
+  //data = 'Uninitialized';
+  private internalData = 'Uninitialized';
+  public get data() {
+    console.log('Getter');
+    return this.internalData;
+  }
+
+  public set data(value) {
+    console.log('Setter');
+    this.internalData = value;
+  }
+
 }
